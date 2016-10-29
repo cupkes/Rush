abb0649342a08212864fe510502114ceb17a213c
cupkes <chris_deColorado@yahoo.com>
2016-10-29 07:13:21 -0700

    updated init files

--- stage/initRushGraph.sh
+++ stage/initRushGraph.sh
@@ -11,8 +11,14 @@
 ###########################################	  
 # redirect stdout and stderr to logger
 exec 1> >(logger -s -t $(basename $0)) 2>&1
+NEO_HOME="/opt/neo4j/neo4j-enterprise-3.0.4"
+cd $NEO_HOME/bin
+./neo4j stop
+sleep 30
 # delete existing graph data
 ./opt/neo4j_support/etc/deleteGraphDB.sh
+./neo4j start
+sleep 30
 # load Rush graph
 ./opt/neo4j_support/etc/loadRushGraph.sh
 
--- stage/loadRushGraph.sh
+++ stage/loadRushGraph.sh
@@ -21,6 +21,7 @@
 # specify the neo4j home and load utility
 NEO4J_HOME="/opt/neo4j/neo4j-enterprise-3.0.4"
 CMD="$NEO4J_HOME/bin/neo4j-shell -file"
+MSG="EXECUTING SCRIPT:"
 #---------------------------------------------------------
 # We begin by loading non-instance, singleton nodes.
 #
@@ -34,27 +35,35 @@ CMD="$NEO4J_HOME/bin/neo4j-shell -file"
 # 
 #---------------------------------------------------------
 # LOADING Patients
+echo "$MSG loadPatients.cql"
 $CMD /opt/neo4j/stage/CQL/loadPatients.cql
 sleep 10 # waiting for indexes to build
 # LOADING Providers
+echo "$MSG lloadProviders.cql"
 $CMD /opt/neo4j/stage/CQL/loadProviders.cql
 sleep 10 # waiting for indexes to build
 # LOADING Diagnosis Codes
+echo "$MSG loadDX.cql"
 $CMD /opt/neo4j/stage/CQL/loadDX.cql
 sleep 10 # waiting for indexes to build
 # LOADING Medication Codes
+echo "$MSG loadRX.cql"
 $CMD /opt/neo4j/stage/CQL/loadRX.cql
 sleep 10 # waiting for indexes to build
 # LOADING Procedure Codes
+echo "$MSG loadPX.cql"
 $CMD /opt/neo4j/stage/CQL/loadPX.cql
 sleep 10 # waiting for indexes to build
 # LOADING Labs
+echo "$MSG loadLabs.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabs.cql
 sleep 10 # waiting for indexes to build
 # LOADING Timeline
+echo "$MSG loadTimeTree.cql"
 $CMD /opt/neo4j/stage/CQL/loadTimeTree.cql
 sleep 10 # waiting for indexes to build
 # LOADING Timeline Seconds
+echo "$MSG loadTimeTreeSeconds.cql"
 $CMD /opt/neo4j/stage/CQL/loadTimeTreeSeconds.cql
 sleep 10 # waiting for indexes to build
 #---------------------------------------------------------
@@ -66,12 +75,15 @@ sleep 10 # waiting for indexes to build
 # 
 #---------------------------------------------------------
 # LOADING Encounters
+echo "$MSG loadEncounters.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounters.cql
 sleep 30 # waiting for indexes to build
 # LOADING Conditions
+echo "$MSG loadConditions.cql"
 $CMD /opt/neo4j/stage/CQL/loadConditions.cql
 sleep 30 # waiting for indexes to build
 # LOADING Lab Requests
+echo "$MSG loadLabRequests.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabRequests.cql
 sleep 30 # waiting for indexes to build
 #---------------------------------------------------------
@@ -92,39 +104,51 @@ sleep 30 # waiting for indexes to build
 # 
 #--------------------------------------------------------- 
 # LOADING Condition Onsets
+echo "$MSG loadConditionOnsets.cql"
 $CMD /opt/neo4j/stage/CQL/loadConditionOnsets.cql
 sleep 30 # waiting for indexes to build
 # LOADING Condition Resolutions
+echo "$MSG loadConditionResolutions.cql"
 $CMD /opt/neo4j/stage/CQL/loadConditionResolutions.cql
 sleep 30 # waiting for indexes to build
 # LOADING Deaths
+echo "$MSG loadDeaths.cql"
 $CMD /opt/neo4j/stage/CQL/loadDeaths.cql
 sleep 30 # waiting for indexes to build
 # LOADING Diagnoses
+echo "$MSG loadDiagnoses.cql"
 $CMD /opt/neo4j/stage/CQL/loadDiagnoses.cql
 sleep 30 # waiting for indexes to build
 # LOADING Encounter Starts
+echo "$MSG loadEncounterStarts.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterStarts.cql
 sleep 30 # waiting for indexes to build
 # LOADING Encounter Ends
+echo "$MSG loadEncounterEnds.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterEnds.cql
 sleep 30 # waiting for indexes to build
 # LOADING Lab Orders
+echo "$MSG loadLabOrders.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabOrders.cql
 sleep 30 # waiting for indexes to build
 # LOADING Lab Collections
+echo "$MSG loadCollections.cql"
 $CMD /opt/neo4j/stage/CQL/loadCollections.cql
 sleep 30 # waiting for indexes to build
 # LOADING Lab Results
+echo "$MSG loadLabResults.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabResults.cql
 sleep 30 # waiting for indexes to build
 # LOADING Procedures
+echo "$MSG loadProcedures.cql"
 $CMD /opt/neo4j/stage/CQL/loadProcedures.cql
 sleep 30 # waiting for indexes to build
 # LOADING Medication Orders
+echo "$MSG loadRXOrders.cql"
 $CMD /opt/neo4j/stage/CQL/loadRXOrders.cql
 sleep 30 # waiting for indexes to build
 # LOADING Vital Observations 
+echo "$MSG loadVitals.cql"
 $CMD /opt/neo4j/stage/CQL/loadVitals.cql
 sleep 30 # waiting for indexes to build
 #---------------------------------------------------------
@@ -137,6 +161,7 @@ sleep 30 # waiting for indexes to build
 #
 #---------------------------------------------------------
 # LOADING Event Label Hierarchy
+echo "$MSG loadEventLabelHierarchy.cql"
 $CMD /opt/neo4j/stage/CQL/loadEventLabelHierarchy.cql
 #---------------------------------------------------------
 # Next we load the relationships
@@ -168,54 +193,79 @@ $CMD /opt/neo4j/stage/CQL/loadEventLabelHierarchy.cql
 #
 #---------------------------------------------------------
 # LOADING Patient to Encounter Relationships
+echo "$MSG loadPatientEncounterRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadPatientEncounterRels.cql
 # LOADING Encounter to Provider Relationships
+echo "$MSG loadEncounterProviderRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterProviderRels.cql
 # LOADING Encounter to Diagnosis Relationships
+echo "$MSG loadEncounterDiagnosisRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterDiagnosisRels.cql
 # LOADING Encounter to Vital Observation Relationships
+echo "$MSG loadEncounterVitalRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterVitalRels.cql
 # LOADING Encounter to Lab Orders
+echo "$MSG loadEncounterLabOrderRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterLabOrderRels.cql
 # LOADING Encounter to Lab Collections
+echo "$MSG loadEncounterLabCollectionRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterLabCollectionRels.cql
 # LOADING Encounter to Lab Results
+echo "$MSG loadEncounterLabResultRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterLabResultRels.cql
 # LOADING Encounter to RX Orders
+echo "$MSG loadEncounterRXOrderRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterRXOrderRels.cql
 # LOADING Encounter to RX Orders
+echo "$MSG loadEncounterStartsRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterStartsRels.cql
 # LOADING Encounter to RX Orders
+echo "$MSG loadEncounterEndsRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterEndsRels.cql
 # LOADING Encounter to Procedures
+echo "$MSG loadProcedureEncounterRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadProcedureEncounterRels.cql
 # LOADING Patient to Condition
+echo "$MSG loadPatientConditionRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadPatientConditionRels.cql
 # LOADING Patient to Deaths
+echo "$MSG loadPatientDeathRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadPatientDeathRels.cql
 # LOADING Diagnosis to DX Codes
+echo "$MSG loadDiagnosisDXRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadDiagnosisDXRels.cql
 # LOADING Condition to DX Codes
+echo "$MSG loadConditionDXRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadConditionDXRels.cql
 # LOADING Procedure to PX Codes
+echo "$MSG loadProcedurePXRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadProcedurePXRels.cql
 # LOADING Provider to Procedure
+echo "$MSG loadProviderProcedureRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadProviderProcedureRels.cql
 # LOADING Provider to Diagnosis
+echo "$MSG loadProviderDiagnosisRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadProviderDiagnosisRels.cql
 # LOADING RX Order to RX Codes
+echo "$MSG loadRXOrderRXRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadRXOrderRXRels.cql
 # LOADING Lab Request to Labs
+echo "$MSG loadLabRequestLabRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabRequestLabRels.cql
 # LOADING Lab Request to Lab Orders
+echo "$MSG loadRequestOrderRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadRequestOrderRels.cql
 # LOADING Lab Request to Lab Collections
+echo "$MSG loadLabRequestCollectionRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabRequestCollectionRels.cql
 # LOADING Lab Request to Lab Results
+echo "$MSG loadRequestResultRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadRequestResultRels.cql
 # LOADING Condition to Condition Onsets
+echo "$MSG loadConditionOnsetRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadConditionOnsetRels.cql
 # LOADING Condition to Condition Resolutions
+echo "$MSG loadConditionResolutionRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadConditionResolutionRels.cql
 
 
@@ -229,6 +279,7 @@ $CMD /opt/neo4j/stage/CQL/loadConditionResolutionRels.cql
 #
 #---------------------------------------------------------
 # LOADING Indicator Event Labels
+echo "$MSG loadIndicatorEventLabels.cql"
 $CMD /opt/neo4j/stage/CQL/loadIndicatorEventLabels.cql
 
 #---------------------------------------------------------
@@ -238,6 +289,7 @@ $CMD /opt/neo4j/stage/CQL/loadIndicatorEventLabels.cql
 #
 #---------------------------------------------------------
 # LOADING Next Event Relationships
+echo "$MSG loadNextEventRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadNextEventRels.cql
 
 #---------------------------------------------------------
@@ -259,31 +311,45 @@ $CMD /opt/neo4j/stage/CQL/loadNextEventRels.cql
 #
 #---------------------------------------------------------
 # LOADING Date start and end in seconds since Jan 1st, 1970
+echo "$MSG loadDayStartAndEndSeconds.cql"
 $CMD /opt/neo4j/stage/CQL/loadDayStartAndEndSeconds.cql
 sleep 30 # waiting for indexes to build
 # LOADING Procedure to Day Relationships
+echo "$MSG loadProcedureDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadProcedureDayRels.cql
 # LOADING Condition Onset to Dday Relationships
+echo "$MSG loadConditionOnsetDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadConditionOnsetDayRels.cql
 # LOADING Condition Resolution to Day Relationships
+echo "$MSG loadConditionResolutionDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadConditionResolutionDayRels.cql
 # LOADING RXOrder to Day Relationships
+echo "$MSG loadRXOrderDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadRXOrderDayRels.cql
 # LOADING Encounter Start to Day Relationships
+echo "$MSG loadEncounterStartDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterStartDayRels.cql
 # LOADING Encounter End to Day Relationships
+echo "$MSG loadEncounterEndDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadEncounterEndDayRels.cql
 # LOADING Lab Order to Day Relationships
+echo "$MSG loadLabOrderDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabOrderDayRels.cql
 # LOADING Lab Collection to Day Relationships
+echo "$MSG loadLabCollectionDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabCollectionDayRels.cql
 # LOADING Lab Result to Day Relationships
+echo "$MSG loadLabResultDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabResultDayRels.cql
 # LOADING Lab Collection to Day Relationships
+echo "$MSG loadLabCollectionDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabCollectionDayRels.cql
 # LOADING Lab Result to Day Relationships
+echo "$MSG loadLabResultDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabResultDayRels.cql
 # LOADING Lab Vital Observation to Day Relationships
+echo "$MSG loadLabCollectionDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadLabCollectionDayRels.cql
 # LOADING Lab Death to Day Relationships
+echo "$MSG loadDeathDayRels.cql"
 $CMD /opt/neo4j/stage/CQL/loadDeathDayRels.cql
